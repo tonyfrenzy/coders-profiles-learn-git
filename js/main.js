6aa6ad8bbc927fb486eccaa1ca00078ed38b0ac4
Ayo <odunet2000@yahoo.com>
2021-02-25 18:13:53 +0100

    Code modifications: 1) main.js (Line 137) - Updated the profileBioHTML.value to display new code in screen
    2) main.js (Line 38) - Set default texts in Githiub profile, LinkedIn profile, and Twitter profile as placeholder
    
    3) main.js (Line 110) - Fixed Bug that prevented subsequent submission of modal form after first failed attempt
    
    4) Style.css (Line 206) - Added media query to make code responsive for smaller screens
    
    5) style.css (Line 69) - Updated css to include relative position

--- js/main.js
+++ js/main.js
@@ -1,6 +1,7 @@
 var profilesCount = function () {
   var profiles = document.querySelectorAll('dl.personProfile');
-	var profilesCount = profiles.length;
+  var profilesCount = profiles.length;
+  console.log(profilesCount);
 	var profilesCountDisplay = document.getElementById('profilesCount');
 	profilesCountDisplay.innerText = profilesCount;
 }
@@ -17,7 +18,6 @@ profilesCount();
 */
 var newProfileModal = document.getElementById("newProfileModal");
 var newProfileBtn = document.getElementById("newProfileBtn");
-
 var profileName = document.getElementById("profileName");
 var profileGithub = document.getElementById("profileGithub");
 var profileLinkedIn = document.getElementById("profileLinkedIn");
@@ -27,138 +27,128 @@ var profileImgSource = document.getElementById("profileImgSource");
 var profileBioHTML = document.getElementById("profileBioHTML");
 var pBioCopy = document.getElementById("pBioCopy");
 var requiredFieldsAlert = document.getElementById("requiredFieldsAlert");
-
-
 var span = document.getElementsByClassName("close")[0];
 var cancelBtn = document.getElementById("cancelBtn");
 
-newProfileBtn.onclick = function() {
+//create new profile
+  newProfileBtn.onclick = function() {
   newProfileModal.style.display = "block";
 
   // Pre-populate for easier form filling
-  profileGithub.value = "https://github.com/---username";
-  profileLinkedIn.value = "https://www.linkedin.com/in/---username-id";
-  profileTwitter.value = "https://twitter.com/---username";
+  profileGithub.placeholder = "https://github.com/---username";
+  profileLinkedIn.placeholder = "https://www.linkedin.com/in/---username-id";
+  profileTwitter.placeholder = "https://twitter.com/---username";
 }
 
 // When the user clicks on <span> (x), close the modal
 span.onclick = function() {
   newProfileModal.style.display = "none";
+  document.getElementById("newProfile").reset();
 }
-cancelBtn.onclick = function() {
+
+//Close modal using the close button
+cancelBtn.onclick = function(event) {
   event.preventDefault();
   hideClearModal();
 }
 /** -- end Modal related -- */
 
-addBtn.onclick = function() {
-  event.preventDefault();
+//Impliment filling form within modal
+addBtn.onclick = function(event) {
+    event.preventDefault();
 
-  if (profileName.value == "" || profileGithub.value == "https://github.com/---username") {
-    requiredFieldsAlert.style.display = "block";
-    return false;
-  }
-
-  /** -- Constructed Tag -- */
-  // <dl class="personProfile">
-  //     <dt class="profileName">Coffee</dt>
-  //         <dd><strong class="pKey">Github</strong>: <span class="pValue">
-  //         <a href="https://github.com/tonyfrenzy" target="_blank">https://github.com/tonyfrenzy</a></span></dd>
-  //         <dd><strong class="pKey">LinkedIn</strong>: <span class="pValue"><a href="#" target="_blank">#</a></span></dd>
-  //         <dd><strong class="pKey">Twitter</strong>: <span class="pValue"><a href="#" target="_blank">#</a></span></dd>
-  //         <dd><img src="images/spatula.gif"></dd>
-
-  //         <dd><strong class="pKey">Bio</strong>: <span class="pValue"> Brag!!! here about your achievemeants. Lorem ipsum dolor sit amet consectetur adipisicing elit. Ad accusantium ex tempore cupiditate consequatur magnam quia, explicabo eaque, architecto, voluptatum perspiciatis. Quas minima nostrum sequi id, odit dolore illo labore.</span></dd>
-  // </dl>
-  //
-  var newProfile = document.createElement("dl");
-  var profileNameNode = document.createElement("dt");
-  var profileGithubNode = document.createElement("dd");
+    var newProfile = document.createElement("dl");
+    var profileNameNode = document.createElement("dt");
+    var profileGithubNode = document.createElement("dd");
     var profileGhNodeStrong = document.createElement("strong");
     var profileGhNodeSpan = document.createElement("span");
     var profileGhNodeA = document.createElement("a");
 
-  var profileLinkedInNode = document.createElement("dd");
+    var profileLinkedInNode = document.createElement("dd");
     var profileLinkedInNodeStrong = document.createElement("strong");
     var profileLinkedInNodeSpan = document.createElement("span");
     var profileLinkedInNodeA = document.createElement("a");
 
-  var profileTwitterNode = document.createElement("dd");
+    var profileTwitterNode = document.createElement("dd");
     var profileTwitterNodeStrong = document.createElement("strong");
     var profileTwitterNodeSpan = document.createElement("span");
     var profileTwitterNodeA = document.createElement("a");
 
-  var profileImageNode = document.createElement("dd");
+    var profileImageNode = document.createElement("dd");
     var profileImageTagNode = document.createElement("img");
 
-  var profileBioNode = document.createElement("dd");
+    var profileBioNode = document.createElement("dd");
     var profileBioNodeStrong = document.createElement("strong");
     var profileBioNodeSpan = document.createElement("span");
 
   // Construct profile HTML
-  newProfile.classList.add('personProfile');
-  newProfile.appendChild(profileNameNode);
-  newProfile.appendChild(profileGithubNode);
+    newProfile.classList.add('personProfile');
+    newProfile.appendChild(profileNameNode);
+    newProfile.appendChild(profileGithubNode);
   	profileGithubNode.appendChild(profileGhNodeStrong);// <dd><strong>Github:</strong>
     profileGithubNode.appendChild(profileGhNodeSpan);  //     <span>
     profileGhNodeSpan.appendChild(profileGhNodeA);     //     <a></a></span></dd>
 
-  newProfile.appendChild(profileLinkedInNode);
+    newProfile.appendChild(profileLinkedInNode);
     profileLinkedInNode.appendChild(profileLinkedInNodeStrong);// <dd><strong>LinkedIn:</strong>
     profileLinkedInNode.appendChild(profileLinkedInNodeSpan);  //     <span>
     profileLinkedInNodeSpan.appendChild(profileLinkedInNodeA); //     <a></a></span></dd>
 
-  newProfile.appendChild(profileTwitterNode);
+    newProfile.appendChild(profileTwitterNode);
     profileTwitterNode.appendChild(profileTwitterNodeStrong);// <dd><strong>Twitter:</strong>
     profileTwitterNode.appendChild(profileTwitterNodeSpan);  //     <span>
     profileTwitterNodeSpan.appendChild(profileTwitterNodeA); //     <a></a></span></dd>
 
-  newProfile.appendChild(profileImageNode);
-    profileImageNode.appendChild(profileImageTagNode);// <dd><img /></dd>
-
-  newProfile.appendChild(profileBioNode);
+    newProfile.appendChild(profileBioNode);
     profileBioNode.appendChild(profileBioNodeStrong);// <dd><strong>Bio:</strong>
     profileBioNode.appendChild(profileBioNodeSpan);  //     <span></span></dd>
+
+    newProfile.appendChild(profileImageNode);
+    profileImageNode.appendChild(profileImageTagNode);// <dd><img /></dd>
   
+    if (profileName.value == "" || profileGithub.value == "https://github.com/---username") {
+      requiredFieldsAlert.style.display = "block";
+    } else {
+  // clear model unfilled form alert
+      requiredFieldsAlert.style.display = "none";
 
+  // Populate profile HTML with extracted data
+      profileNameNode.innerText = profileName.value;
+      profileGhNodeStrong.innerText = 'Github :';
+      profileGhNodeA.href = profileGithub.value;
+      profileGhNodeA.innerText = profileGithub.value;
+      profileGhNodeA.target = '_blank';
 
-// Populate profile HTML with extracted data
-  profileNameNode.innerText = profileName.value;
-  profileGhNodeStrong.innerText = 'Github :';
-    profileGhNodeA.href = profileGithub.value;
-    profileGhNodeA.innerText = profileGithub.value;
-    profileGhNodeA.target = '_blank';
+      profileLinkedInNodeStrong.innerText = 'LinkedIn :';
+      profileLinkedInNodeA.href = profileLinkedIn.value;
+      profileLinkedInNodeA.innerText = profileLinkedIn.value;
+      profileLinkedInNodeA.target = '_blank';
 
-  profileLinkedInNodeStrong.innerText = 'LinkedIn :';
-    profileLinkedInNodeA.href = profileLinkedIn.value;
-    profileLinkedInNodeA.innerText = profileLinkedIn.value;
-    profileLinkedInNodeA.target = '_blank';
+      profileTwitterNodeStrong.innerText = 'Twitter :';
+      profileTwitterNodeA.href = profileTwitter.value;
+      profileTwitterNodeA.innerText = profileTwitter.value;
+      profileTwitterNodeA.target = '_blank';
 
-  profileTwitterNodeStrong.innerText = 'Twitter :';
-    profileTwitterNodeA.href = profileTwitter.value;
-    profileTwitterNodeA.innerText = profileTwitter.value;
-    profileTwitterNodeA.target = '_blank';
+      profileImageTagNode.src = profileImgSource.value;
 
-  profileImageTagNode.src = profileImgSource.value;
+      profileBioNodeStrong.innerText = 'Bio :';
+      profileBioNodeSpan.innerText = profileBio.value;
 
-  profileBioNodeStrong.innerText = 'Bio :';
-    profileBioNodeSpan.innerText = profileBio.value;
+      console.log(newProfile);
+      profileBioHTML.value = (newProfile.innerHTML);
 
-    console.log(newProfile);
-    profileBioHTML.value = newProfile;
-    // profileBioHTML.innerText = newProfile.decode();
+      var profiles = document.getElementById("profiles");
+      var fChild = profiles.firstChild;
+      profiles.insertBefore(newProfile, fChild);
 
-  var profiles = document.getElementById("profiles");
-  var fChild = profiles.firstChild;
-  profiles.insertBefore(newProfile, fChild);
+    // Update profiles count
+      profilesCount();
 
-// Update profiles count
-  profilesCount();
+    // Clear form and hide modal.
+      hideClearModal();
 
-// Clear form and hide modal.
-  hideClearModal();
-
-  pBioCopy.style.display = "block";
+      pBioCopy.style.display = "block";
+    }
 }
 
 // When the user clicks anywhere outside of the modal, close it
